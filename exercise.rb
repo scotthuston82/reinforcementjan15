0a323dfa4254235ecf350a5b81080ae745cda0ff
Scott <scotthuston1982@gmail.com>
2018-01-16 09:47:21 -0500

    finished

--- exercise.rb
+++ exercise.rb
@@ -5,12 +5,25 @@ ballots = [{1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'},
            {1 => 'Lucky', 2 => 'Felix', 3 => 'Bella'},
            {1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
 
+totals ={}
 contestants = []
 
-ballots.each do |hash|
-  hash.values.each do |name|
+ballots.each do |ballot|
+  ballot.values.each do |name|
     contestants << name
   end
 end
 
 contestants.uniq!
+
+contestants.each do |contestant|
+  totals[contestant] = 0
+end
+
+ballots.each do |ballot|
+  ballot.each do |key, value|
+    totals[value] += 4 - key
+  end
+end
+
+p totals.sort_by {|_key, value| value}.reverse
